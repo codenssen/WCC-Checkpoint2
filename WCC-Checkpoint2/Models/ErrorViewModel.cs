59379f2fc69a8089fcdd81b03e32dbe440df6490
ABT Aurelien <aurelien@abrassart.net>
2024-08-28 09:46:03 +0200

    Ajoutez des fichiers projet.

---
+++ WCC-Checkpoint2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WCC_Checkpoint2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
